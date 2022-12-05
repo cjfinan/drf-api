4e9605d717a8085bc587462bea883d11f7e0f787
cjfinan <cjfinan98@gmail.com>
2022-12-05 12:59:12 +0000

    [ADD] posts app with model, serializers, urls, PostList view

--- posts/serializers.py
+++ posts/serializers.py
@@ -6,19 +6,19 @@ class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    profile_image = serailizers.ReadOnlyField(source='owner.profile.image.url')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
-            raise serailizers.ValidationError(
+            raise serializers.ValidationError(
                 'Image sizer larger than 2MB!'
             )
         if value.image.width > 4096:
-            raise serailizers.ValidationError(
+            raise serializers.ValidationError(
                 'Image width larger than 4096px'
             )
         if value.image.height > 4096:
-            raise serializer.ValidationError(
+            raise serializers.ValidationError(
                 'Image height larger than 4096px'
             )
         return value
@@ -30,6 +30,7 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = [
-            'id', 'owner', 'created_at', 'update_at', 'title',
+            'id', 'owner', 'profile_id', 'profile_image',
+            'created_at', 'title',
             'content', 'image', 'is_owner', 'image_filter'
         ]
